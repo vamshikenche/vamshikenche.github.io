5e06ba05c178d240024000525c6dd1f0702677c2
vamshi krishna kenche <vamshi550@gmail.com>
2018-11-15 21:48:24 +0530

    2

--- doWork.js
+++ doWork.js
@@ -1,15 +1,47 @@
-self.addEventListener('message', function(e){
+self.addEventListener('message', function(e) {
     var data = e.data;
-  switch (data.cmd) {
-    case 'start':
-      self.postMessage('WORKER STARTED: ' + data.msg);
-      break;
-    case 'stop':
-      self.postMessage('WORKER STOPPED: ' + data.msg +
-                       '. (buttons will no longer work)');
-      self.close(); // Terminates the worker.
-      break;
-    default:
-      self.postMessage('Unknown command: ' + data.msg);
-  };
+    switch (data.cmd) {
+        case 'start':
+            self.postMessage('WORKER STARTED: ' + data.msg);
+            break;
+        case 'stop':
+            self.postMessage('WORKER STOPPED: ' + data.msg +
+                '. (buttons will no longer work)');
+            self.close(); // Terminates the worker.
+            break;
+        default:
+            self.postMessage('Unknown command: ' + data.msg);
+    };
 }, false);
+
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+    '/',
+    '/page2.html',
+    '/home.html'
+];
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
